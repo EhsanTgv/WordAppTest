23a7217025ac365fd1066e5ab50ab14093e40abf
EhsanTgv <EhsanTgv@gmail.com>
2022-01-22 13:20:42 +0300

    Starter Project

--- app/src/main/java/com/taghavi/wordapptest/MainActivity.kt
+++ app/src/main/java/com/taghavi/wordapptest/MainActivity.kt
@@ -2,10 +2,24 @@ package com.taghavi.wordapptest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.taghavi.wordapptest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        // Sets the LinearLayoutManager of the recyclerview
+        recyclerView.layoutManager = GridLayoutManager(this, 4)
+        recyclerView.adapter = LetterAdapter()
     }
+
 }
